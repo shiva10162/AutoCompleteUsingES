28962d567cf0a0a1dd80ac6de5af12eabf4a4b22
Shiva Tiwari <shiva10162@gmail.com>
2021-05-17 22:53:41 +0530

    - updating users list to string list

--- src/main/java/com/ps/autocomplete/controller/UserController.java
+++ src/main/java/com/ps/autocomplete/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,31 +20,35 @@ public class UserController {
   private UserService userService;
 
   @GetMapping(path = "/list")
+  @ResponseBody
   public List<User> getAllUsers() {
     return userService.listAll();
   }
 
   @GetMapping(path = "/search")
+  @ResponseBody
   public List<User> searchUsers(@RequestParam String keywords) {
     return userService.search(keywords);
   }
 
   @GetMapping(path = "/delete")
+  @ResponseBody
   public String deleteUsers() {
     userService.deleteAllUsers();
     return "All Users Deleted!";
   }
 
   @GetMapping(path = "/find")
-  public List<User> searchEmployeebyCountry(
-      @RequestParam(value="term", required = false, defaultValue="") String query) {
+  @ResponseBody
+  public List<String> searchEmployeebyCountry(
+      @RequestParam(value = "term", required = false, defaultValue = "") String query) {
 
     if (StringUtils.isEmpty(query)) {
-      return getAllUsers().stream()
+      return getAllUsers().stream().map(User::getFullName)
           .limit(15)
           .collect(Collectors.toList());
     } else {
-      return searchUsers(query);
+      return searchUsers(query).stream().map(User::getFullName).collect(Collectors.toList());
     }
   }
 
